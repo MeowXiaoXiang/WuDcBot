3e45ceba49e894675d951e5ed86b35efba3e92b7
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-02-07 20:05:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ logger.add(new logger.transports.Console, {
 logger.level = "debug";
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: process.env.token,
+   token: auth.token,
    autorun: true
 });
 bot.on("ready", function (evt) {
