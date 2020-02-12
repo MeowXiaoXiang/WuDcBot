3744b922c40f2e346c990445d3878d391033ed6e
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-02-12 20:44:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,9 @@ if (message.substring(0, 1) == '!') {
 			case 'r6help':
 			bot.sendMessage({to: channelID,message: '@everyone 打開https://dcsebot.q20001116.repl.run/ 即可啟動分隊系統　使用時網頁不可關閉\r\n分隊系統目前支援 3人到10人分組\r\n!r6join 進入分組名單 <--先使用\r\n!r6list 查看已經入人員名單\r\n!r6clear 清除人員名單\r\n!r6ran 進行分組 <---等都加入分組名單後使用\r\n\r\n若有BUG直接PM <@476422785833631749> '});
 			break;
+			case 'user':
+			bot.sendMessage({to: channelID,message: 'user：' + user + '\r\nuserID：' + userID + '\r\nchannelID：' + channelID + '\r\nevt：' + evt});
+			break;
 			case '':
 			break;
          }
