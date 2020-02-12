fc7f4fbcc639c743a368df72b1099edb1a467ca5
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-02-12 18:10:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ if (message.substring(0, 1) == '!') {
 			break;
 			case 'r6':
 			case 'R6':
+			case '阿6':
+			case '啊6':
 			bot.sendMessage({to: channelID,message: '<@&614789432247320576> 組隊缺人～揪滿開團！'});
 			var ranr6 = Math.floor(Math.random()*15)+1;
 			if (ranr6 == 15){bot.sendMessage({to: channelID,message: '全國最大Ｒ２０線上商城上線啦！ \r\n欲求不滿這內洽：<@' + userID + '>'});}
@@ -49,10 +51,6 @@ if (message.substring(0, 1) == '!') {
 			break;
 			case 'r6help':
 			bot.sendMessage({to: channelID,message: '@everyone 打開https://dcsebot.q20001116.repl.run/ 即可啟動分隊系統　使用時網頁不可關閉\r\n分隊系統目前支援 3人到10人分組\r\n!r6join 進入分組名單 <--先使用\r\n!r6list 查看已經入人員名單\r\n!r6clear 清除人員名單\r\n!r6ran 進行分組 <---等都加入分組名單後使用\r\n\r\n若有BUG直接PM <@476422785833631749> '});
-			break;
-			case '阿6':
-			case '啊6':
-bot.sendMessage({to: channelID,message: '<@&614789432247320576> 組隊缺人～揪滿開團！'});
 			break;
 			case '':
 			break;
