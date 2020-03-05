6336a85e7f8e3600c70b201a3955d297c1d94860
Wuchieh <60553652+Wuchieh@users.noreply.github.com>
2020-03-05 14:34:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.on("ready", function (evt) {
     logger.info(bot.username + " - (" + bot.id + ")");
 });
 bot.on("message", function (user, userID, channelID, message, evt) {
-if (message.substring(0, 1) == '!') {
+if (message.substring(0, 1) == '/') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
 	var msg1 = args[1];
